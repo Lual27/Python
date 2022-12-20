36f47f0e8e04b0ebba950b712c767b51e2e32e0b
Deng Lual <ldeng8284@gmail.com>
2022-12-20 21:16:09 +0300

    Added new

---
+++ dir2/classes.py
@@ -0,0 +1,18 @@
+class item:
+  def calculate_total_price(self,x,y):
+    return x * y
+
+
+item1 = item()
+item1.name = "Phone"
+item1.price =  100
+item1.quantity = 5
+
+print(item1.calculate_total_price(item1.price,item1.quantity))
+
+item2 = item()
+item2.name = "Laptop"
+item2.price =  1000
+item2.quantity = 4
+
+print(item2.calculate_total_price(item2.price,item2.quantity))
