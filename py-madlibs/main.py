164b27f386db987fb61e7a71efdba5c3a75dadbc
Deng Lual <ldeng8284@gmail.com>
2023-01-04 08:47:08 +0300

    guess game

--- py-madlibs/main.py
+++ py-madlibs/main.py
@@ -3,6 +3,6 @@ def guess(x):
   random_number = random.randint(1,x)
   guess = 0
   while guess != random_number:
-      guess = input(f"Guess a number between 1 and {x}": )
+      guess = input(f"Guess a number between 1 and {x}: ")
       print(guess)
 guess(10)
